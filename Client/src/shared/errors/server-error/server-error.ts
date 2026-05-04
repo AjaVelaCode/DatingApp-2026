131bb72d5428b93b09a431ad785f280511473c6b
AjaVelaCode <andrijavela@gmail.com>
2026-05-04 18:35:01 +0200

    End of section 7

---
+++ Client/src/shared/errors/server-error/server-error.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { ApiError } from '../../../types/error';
+
+@Component({
+  selector: 'app-server-error',
+  imports: [],
+  templateUrl: './server-error.html',
+  styleUrl: './server-error.css',
+})
+export class ServerError {
+  protected error: ApiError;
+  protected showDetails = false;
+
+  constructor() {
+    this.error = history?.state?.['error'];
+    }
+
+    detailsToogle(){
+      this.showDetails = !this.showDetails;
+    }
+}
